d0f3f54e9d599a9248bd1e4a6c94e2967003f2a5
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-21 12:38:21 +0530

    routing in action

--- InAndOut/Controllers/AppointmentController.cs
+++ InAndOut/Controllers/AppointmentController.cs
@@ -12,5 +12,10 @@ namespace InAndOut.Controllers
         {
             return View();
         }
+
+        public IActionResult Details(int id)
+        {
+            return Ok("You have entered id = {id} " + id);
+        }
     }
 }
