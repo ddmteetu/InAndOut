5d47f6871dc3a8ac6b13031a8adad065602136b1
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-21 12:25:56 +0530

    creating our own view

--- InAndOut/Controllers/AppointmentController.cs
+++ InAndOut/Controllers/AppointmentController.cs
@@ -10,9 +10,7 @@ namespace InAndOut.Controllers
     {
         public IActionResult Index()
         {
-            // return View();
-            string todaysDate = DateTime.Now.ToShortDateString();
-            return Ok(todaysDate);
+            return View();
         }
     }
 }
