7adfc151d264cc8309240bc2bf570290e99c8fea
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-27 17:37:55 +0530

    designing the create item page

--- InAndOut/Models/Item.cs
+++ InAndOut/Models/Item.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace InAndOut.Models
 {
@@ -12,6 +13,7 @@ namespace InAndOut.Models
         public int Id { get; set; }
         public string Borrower { get; set; }
         public string Lender { get; set; }
+        [DisplayName("Item name")]
         public string ItemName { get; set; }
     }
 }
