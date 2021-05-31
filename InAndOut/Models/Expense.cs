40c2ad46b436dc157f67a44c226c1756e53cab05
Vishvendra Singh <ddmteetu@gmail.com>
2021-05-31 12:17:42 +0530

    updated Expenses table by adding a Foreign Key to the ExpenseCategories table

--- InAndOut/Models/Expense.cs
+++ InAndOut/Models/Expense.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -17,5 +18,10 @@ namespace InAndOut.Models
         [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Amount must be greater than 0!")]
         public int Amount { get; set; }
+
+        [DisplayName("Expense Category")]
+        public int ExpenseCategoryId { get; set; }
+        [ForeignKey("ExpenseCategoryId")]
+        public virtual ExpenseCategory ExpenseCategory { get; set; }
     }
 }
